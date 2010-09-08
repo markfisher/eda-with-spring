2be455331afeb7df03745aab7e7fa4475cba46b3
Mark Fisher <mark.ryan.fisher@gmail.com>
2010-09-08 18:17:18 +0200

    renamed polling-context to task-context

--- src/main/java/eda/file/demo/FilePollingTaskDemo.java
+++ src/main/java/eda/file/demo/FilePollingTaskDemo.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class FilePollingTaskDemo {
 
 	public static void main(String... args) {
-		new ClassPathXmlApplicationContext("polling-context.xml", FilePollingTaskDemo.class);
+		new ClassPathXmlApplicationContext("task-context.xml", FilePollingTaskDemo.class);
 	}
 
 }
