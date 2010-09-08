3fbdab1fe7359777dd284ef1a78808edea4d8fce
Mark Fisher <mark.ryan.fisher@gmail.com>
2010-09-08 17:34:00 +0200

    completed refactoring of event example

--- src/main/java/eda/event/demo/CustomEventDemo.java
+++ src/main/java/eda/event/demo/CustomEventDemo.java
@@ -1,20 +1,14 @@
 package eda.event.demo;
 
-import org.junit.runner.RunWith;
-
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import eda.event.CustomEvent;
 
-@ContextConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
 public class CustomEventDemo {
 
 	public static void main(String... args) {
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("custom-context", CustomEventDemo.class);
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("custom-context.xml", CustomEventDemo.class);
 		context.start();
 		context.publishEvent(new CustomEvent("foo"));
 		context.stop();
